67c32532506ccf1ae032aa3cd604ba38127440ce
Garrett LeSage <garrett@lesage.us>
2017-02-28 14:03:59 +0100

    take lshw HTML output and format to YAML

---
+++ devices2yaml.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'yaml'
+require 'oga'
+
+htmldoc = File.read('hardware.html')
+
+doc = Oga.parse_html(htmldoc)
+
+system_info = []
+
+doc.css('.node').each do |item|
+  device_info = {}
+
+  item.css('tr').each do |info|
+    key = info.css('.first').text.strip.sub(/:$/, '')
+    val = info.css('.second').text.strip
+
+    case key
+    when 'capabilities'
+      val = val.split(' ')
+    when 'configuration'
+      val_obj = []
+      val.split(' ').each do |config|
+        config_info = {}
+        config_key, config_val = config.split('=')
+
+        # Convert to a number if appropriate
+        config_val = config_val.to_i if config_val.to_i.to_s == config_val
+
+        config_info[config_key] = config_val
+
+        val_obj.push config_info
+      end
+      val = val_obj
+    when 'id'
+      device_info['class'] = val.split(':').first
+    else
+      # Convert to a number if appropriate
+      val = val.to_i if val.to_i.to_s == val
+    end
+
+    device_info[key] = val unless key.empty?
+  end
+
+  system_info.push device_info
+end
+
+puts system_info.to_yaml
